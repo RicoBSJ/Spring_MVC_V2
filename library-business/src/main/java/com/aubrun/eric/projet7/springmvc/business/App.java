26cbc5224bcc4a44c3c52e79f99527f07f88a808
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-12-04 00:05:32 +0100

    FirstCommit

---
+++ library-business/src/main/java/com/aubrun/eric/projet7/springmvc/business/App.java
@@ -0,0 +1,13 @@
+package com.aubrun.eric.projet7.springmvc.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ library-consumer/src/main/java/com/aubrun/eric/projet7/springmvc/consumer/App.java
@@ -0,0 +1,13 @@
+package com.aubrun.eric.projet7.springmvc.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
