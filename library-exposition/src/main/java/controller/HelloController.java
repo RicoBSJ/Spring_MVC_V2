67f88f7a50ca41326d9a08d793827b0d587e4758
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-12-04 01:16:40 +0100

    SecondCommit

---
+++ library-exposition/src/main/java/controller/HelloController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("")
+@Controller
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHelloWorld(ModelMap modelMap) {
+
+        // add attribute to load modelMap
+        modelMap.addAttribute("message",
+                "Hello World and Welcome to Spring MVC!");
+
+        // return the name of the file to be loaded "hello_world.jsp"
+        return "hello_world";
+    }
+}
