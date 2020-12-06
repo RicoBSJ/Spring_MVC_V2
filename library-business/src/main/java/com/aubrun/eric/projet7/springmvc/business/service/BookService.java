0c79355ffe78fae53a5c76536daae7b204d6f99b
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2020-12-06 01:17:17 +0100

    KeepOnTryToDisplayListBook

---
+++ library-business/src/main/java/com/aubrun/eric/projet7/springmvc/business/service/BookService.java
@@ -0,0 +1,23 @@
+package com.aubrun.eric.projet7.springmvc.business.service;
+
+import com.aubrun.eric.projet7.springmvc.consumer.BookConsumer;
+import com.aubrun.eric.projet7.springmvc.model.Books;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BookService {
+
+    private final BookConsumer bookConsumer;
+
+    public BookService(BookConsumer bookConsumer) {
+        this.bookConsumer = bookConsumer;
+    }
+
+    public ResponseEntity<Books> findAll() {
+
+        return bookConsumer.findAllBooks();
+    }
+}
--- library-exposition/src/main/java/com/aubrun/eric/projet7/springmvc/exposition/controller/BookController.java
+++ library-exposition/src/main/java/com/aubrun/eric/projet7/springmvc/exposition/controller/BookController.java
@@ -1,22 +1,31 @@
 package com.aubrun.eric.projet7.springmvc.exposition.controller;
 
-import com.aubrun.eric.projet7.springmvc.consumer.BookConsumer;
+import com.aubrun.eric.projet7.springmvc.business.service.BookService;
 import com.aubrun.eric.projet7.springmvc.model.Books;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BookController {
 
-    private final BookConsumer bookConsumer;
+    private final BookService bookService;
 
-    public BookController(BookConsumer bookConsumer) {
-        this.bookConsumer = bookConsumer;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
-    @GetMapping("/book")
-    public String showForm() {
-        Books books = bookConsumer.findAllBooks().getBody();
+    @RequestMapping(value = "/book", method = RequestMethod.GET)
+    public String printHelloWorld(ModelMap modelMap) {
+
+        // add attribute to load modelMap
+        modelMap.addAttribute("message",
+                "Hello World and Welcome to Spring MVC!");
+        Books books = bookService.findAll().getBody();
+        // return the name of the file to be loaded "hello_world.jsp"
         return "book";
     }
+
 }
