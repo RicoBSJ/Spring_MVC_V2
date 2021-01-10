f5fb052e3f5d5c3cbd25bd378f2fa302e455f79b
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2021-01-10 16:13:27 +0100

    MoveAddBorrowingIntoAddUserAccount

--- library-consumer/src/main/java/com/aubrun/eric/projet7/springmvc/consumer/UserAccountConsumer.java
+++ library-consumer/src/main/java/com/aubrun/eric/projet7/springmvc/consumer/UserAccountConsumer.java
@@ -1,9 +1,15 @@
 package com.aubrun.eric.projet7.springmvc.consumer;
 
 import com.aubrun.eric.projet7.springmvc.model.UserAccount;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
 
 @Component
 public class UserAccountConsumer {
@@ -14,15 +20,31 @@ public class UserAccountConsumer {
         this.restTemplate = restTemplate;
     }
 
-    public ResponseEntity<UserAccount> addUserAccount(UserAccount userAccount){
+    /*public ResponseEntity<UserAccount> addUserAccount(UserAccount userAccount){
         return restTemplate.postForEntity("http://localhost:8081/biblio-api/api/auth/signup", userAccount, UserAccount.class);
+    }*/
+
+    public ResponseEntity<UserAccount> addUserAccount(UserAccount userAccount) {
+        String accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtb2QiLCJpYXQiOjE2MTAyMzIyMjIsImV4cCI6MTYxMDMxODYyMn0.lfrryLWk9Z5fTmT19XAEDClrO2c-KvFQ8zQJkmOShK-9wOZjV6-3O_wo0qaL5UkDkMcoBDPebPL_hWQSjXY7fw";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer" + accessToken);
+        HttpEntity<HttpHeaders> entity = new HttpEntity<HttpHeaders>(headers);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpRequest(
+                .queryParam("branch", branch)
+                .queryParam("customerInternalCode", internalCode)
+                .queryParam("hierarchy", hierarchy);
+
+        return Optional.ofNullable(restTemplate.exchange(builder))
+        HttpMethod.GET, entity,
+                CustomerRegistrationDTO.class).getBody());
     }
 
-    public ResponseEntity<UserAccount> addConnectedUser(UserAccount userAccount){
+    public ResponseEntity<UserAccount> addConnectedUser(UserAccount userAccount) {
         return restTemplate.postForEntity("http://localhost:8081/biblio-api/api/auth/signin", userAccount, UserAccount.class);
     }
 
-    public ResponseEntity<UserAccount> userById(int userId){
+    public ResponseEntity<UserAccount> userById(int userId) {
         return restTemplate.getForEntity("http://localhost:8081/biblio-api/users/{userId}", UserAccount.class);
     }
 }
